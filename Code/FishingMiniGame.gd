54d71c4b4266ed53db1b1cddebf2ceb506388c5c
bymaslol <53442778+bymaslol@users.noreply.github.com>
2024-04-21 02:42:16 -0500

    debug + formatting errors

--- Code/FishingMiniGame.gd
+++ Code/FishingMiniGame.gd
@@ -1,25 +1,30 @@
 extends Control
 
-var moving_speed = 100 # pixels per second
-var max_x = 800 # max x-value of the moving line
-var min_x = 100 # minimum x-value of the moving line
-var direction = 1 # start moving right
+# Define the speed at which the line moves and the boundaries within which it can move
+var moving_speed = 100  # pixels per second
+var max_x = 800  # maximum x position of the moving line
+var min_x = 100  # minimum x position of the moving line
+var direction = 1  # initial direction to move (1 for right, -1 for left)
 
-func_ _ready():
-    # Set up the update function
-    set_process(true)
+# This function is called when the node is added to the scene for the first time
+func _ready():
+	# Enable the processing of this node, which allows _process to be called
+	set_process(true)
 
-func_ _process(delta):
-    var current_x = get_node("../MovingLine").get_position().x
-    if current_x >= max_x or current_x <= min_x:
-        direction *= -1
-    current_x += direction * moving_speed * delta
-    get_node("../MovingLine").set_position(Vector(2, current_x, 250))
+# This function is called every frame, and 'delta' is the elapsed time since the previous frame
+func _process(delta):
+	var current_x = get_node("MovingLine").position.x  # Get the current x position of the line
+	# Reverse direction if the line reaches the boundaries
+	if current_x >= max_x or current_x <= min_x:
+		direction *= -1
+	# Update the position of the line based on the direction and speed
+	current_x += direction * moving_speed * delta
+	get_node("MovingLine").position.x = current_x  # Set the new position
 
-    # Check if the user has clicked the mouse
-    if Input.is_action_just_pressed("mouse_click"):
-        # Check if the click was while the line was within the blue area
-        if current_x in range(350, 500): # blue area range
-            print("Fish Caught")
-        else:
-            print("Fish Escaped")
+	# Check for mouse clicks to determine if the fish is caught
+	if Input.is_action_just_pressed("mouse_click"):
+		# Check if the line is within the blue zone when clicked
+		if current_x >= 350 and current_x <= 500:
+			print("Fish Caught")
+		else:
+			print("Fish Escaped")
