c7fbcf61f89704181814d6bad04ec6ff4c74667f
Lucas Noack <lucasnoack76@gmail.com>
2024-04-21 22:49:02 -0500

    minor adjustments on PlayerCharacter

--- Code/fishingCast.gd
+++ Code/fishingCast.gd
@@ -8,7 +8,8 @@ var _rotSpeed: float = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	coroutine(_timerBack, _timerForward)	
+	#coroutine(_timerBack, _timerForward)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
